0422723a943f01c54fc5978298dc683ef3bbb4c0
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-09 15:05:04 +0100

    complete palidrome word task

--- main.js
+++ main.js
@@ -19,14 +19,31 @@ Ho bisogno di parametri?
 Devo restituire un valore?
 Se sì, di che tipo? */
 
-const word = "antonio"
-let reversedWord = ""
+//chiediamo all'user di scrivere una parola in minuscolo
+const word = prompt("type a word in lowercase")
+console.log(word);
 
-for (let i=word.length -1 ; i>=0; i--){
+//apriamo una funzione chiamata itsPalindrome il cui parametro è la parola scritta dall'user
+function itsPalindrome(word) {
+    //creiamo una variabile che ci servirà per metterci la parola al contrario
+    let reversedWord = ""
     
-    reversedWord += word[i];
-}
-console.log(reversedWord);
+    //apriamo un ciclo for che leggerà al contrario la parola dell'user
+    for (let i = word.length - 1; i >= 0; i--) {
+        //creiamo una variabile per associare gli index della parola
+        const char = word[i]
+        //scriviamo che la variabile della parola al contrario è data dalla somma di se stessa e degli indici della parola dell'user
+        reversedWord += char  
+    } 
+    //finiamo con un return che ci darà un valore booleano (true o false) che dipenderà se la parola dell'user è uguale, in tutto e per tutto, alla parola stessa al contrario
+    return reversedWord===word
+    
+} 
+    //stampiamo il risultato della funzione
+    console.log(itsPalindrome(word));
+    
+
+
 
 
 
